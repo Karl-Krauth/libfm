e78d7cf43b45bc4b89b653c0291ab65953c364a0
Karl Krauth <karl.krauth@gmail.com>
2020-03-09 16:44:21 -0700

    Fixed top level relative import.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """The LibFM factorization library."""
 try:
-    from bin import pyfm
+    from .bin import pyfm
 except ImportError as error:
     raise "Could not find pyfm package. You probably need to run make in the libfm directory."
