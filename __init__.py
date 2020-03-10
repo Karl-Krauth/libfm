477cf49aa27967ce8e40ef79cdb8530f828e0818
Karl Krauth <karl.krauth@gmail.com>
2020-03-09 18:24:27 -0700

    Fix pylint.

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 try:
     from .bin import pyfm
 except ImportError as error:
-    raise "Could not find pyfm package. You probably need to run make in the libfm directory."
+    raise 'Could not find pyfm package. You probably need to run make in the libfm directory.'
