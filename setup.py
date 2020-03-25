6522b111a9b92076f4bb41e20e130336ef2168a0
Karl Krauth <karl.krauth@gmail.com>
2020-03-25 15:34:03 +1100

    pip install actually works for real now.

--- setup.py
+++ setup.py
@@ -2,29 +2,14 @@ import glob
 import subprocess
 
 from setuptools import find_packages, setup
-from setuptools.command.develop import develop
-from setuptools.command.egg_info import egg_info
-from setuptools.command.install import install
-
-# This is a hack to add the compiled module to the
-# data files after we run the makefile. This is because
-# the name of the compiled file varies based on the platform
-# it's compiled on.
-data_files = []
+from setuptools.command.build_py import build_py
 
 
-class CustomInstallCommand(install):
+class CustomBuildCommand(build_py):
     def run(self):
         subprocess.check_call(['make'])
-        data_files.append(('pyfm', glob.glob('bin/pyfm*')))
-        install.run(self)
-
-
-class CustomDevelopCommand(develop):
-    def run(self):
-        subprocess.check_call(['make'])
-        data_files.append(('pyfm', glob.glob('bin/pyfm*')))
-        develop.run(self)
+        # scripts.append(glob.glob('pyfm/pyfm*')[0])
+        super().run()
 
 
 setup(
@@ -34,10 +19,10 @@ setup(
     author_email='karl.krauth@gmail.com',
     description='A pybind11 wrapper for libfm.',
     packages=find_packages(),
-    data_files=data_files,
+    # package_data={'pyfm': ['pyfm*']},
+    include_package_data=True,
     cmdclass={
-        'install': CustomInstallCommand,
-        'develop': CustomDevelopCommand,
+        'build_py': CustomBuildCommand,
     },
     install_requires=[
         'pybind11',
